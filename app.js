ae88b044bfdd876e208b499dc56663ce9075c135
AsadullahNagori <164639593+AsadullahNagori@users.noreply.github.com>
2024-05-14 13:00:10 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+"use strict";
+//assidment=2
+//storing the person name in a variabel
+let personName = "Asadulllah";
+//printing a message to the name
+console.log(`Hellow ${personName} would you like to learn some Python today?`);
---
+++ app.ts
@@ -0,0 +1,8 @@
+//assidment=2
+
+//storing the person name in a variabel
+
+let personName:string="Asadulllah";
+//printing a message to the name
+
+console.log(`Hellow ${personName} would you like to learn some Python today?` );
